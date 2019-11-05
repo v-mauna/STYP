302f6a2bb71a9a0e3b583d618ee16ee16b26b47e
Vanessa Mauna <vmauna@aol.com>
2019-11-05 11:05:16 -0500

    vanessa: Created User,Address models. Updated db/models/index file to include each plus association.

--- server/db/models/user.js
+++ server/db/models/user.js
@@ -10,37 +10,33 @@ const User = db.define('user', {
   },
   password: {
     type: Sequelize.STRING,
-    // Making `.password` act like a func hides it when serializing to JSON.
-    // This is a hack to get around Sequelize's lack of a "private" option.
     get() {
       return () => this.getDataValue('password')
     }
   },
   salt: {
     type: Sequelize.STRING,
-    // Making `.salt` act like a function hides it when serializing to JSON.
-    // This is a hack to get around Sequelize's lack of a "private" option.
     get() {
       return () => this.getDataValue('salt')
     }
   },
   googleId: {
     type: Sequelize.STRING
+  },
+  isAdmin: {
+    type: Sequelize.BOOLEAN
   }
 })
 
 module.exports = User
 
-/**
- * instanceMethods
- */
+//instance methods:
+
 User.prototype.correctPassword = function(candidatePwd) {
   return User.encryptPassword(candidatePwd, this.salt()) === this.password()
 }
 
-/**
- * classMethods
- */
+// class methods:
 User.generateSalt = function() {
   return crypto.randomBytes(16).toString('base64')
 }
@@ -53,9 +49,8 @@ User.encryptPassword = function(plainText, salt) {
     .digest('hex')
 }
 
-/**
- * hooks
- */
+//hooks
+
 const setSaltAndPassword = user => {
   if (user.changed('password')) {
     user.salt = User.generateSalt()
