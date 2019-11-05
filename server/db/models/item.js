122ce1b8ebd494697eb4a774e54dc190a0ee5295
Olivia Sztanga <oliviasztanga@gmail.com>
2019-11-05 11:55:06 -0500

    created item and category models; added default cereal image in ./public

---
+++ server/db/models/item.js
@@ -0,0 +1,32 @@
+const Sequelize = require('sequelize')
+const db = require('../db')
+
+const Item = db.define('item', {
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  price: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    validate: {
+      min: 0
+    }
+  },
+  stock: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    validate: {
+      min: 0
+    }
+  },
+  description: {
+    type: Sequelize.TEXT
+  },
+  photos: {
+    type: Sequelize.ARRAY(Sequelize.STRING),
+    defaultValue: ['cerealPlaceholder.jpg']
+  }
+})
+
+module.exports = Item
