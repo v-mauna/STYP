83b3c88453f7689dfb4d2284992b859bee6f0fba
LinaD <lina.dinh262@gmail.com>
2019-11-07 11:20:56 -0500

    add itemcard component

--- client/components/itemsList.js
+++ client/components/itemsList.js
@@ -1,18 +1,23 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 
-const ItemsList = props => {
-  return (
-    <div>
-      <Link to={`/items/${props.item.id}`}>
-        <h5>{props.item.name}</h5>
-      </Link>
-      <button type="button" onClick="">
-        Add to Cart
-      </button>
-      <img height="100px" width="100px" src={props.item.imageUrl} />
-    </div>
-  )
+class AllCereals extends React.Component {
+  componentDidMount() {
+    this.props.getCereals()
+  }
+  render() {
+    return (
+      <div>
+        <Link to={`/items/${props.item.id}`}>
+          <h5>{props.item.name}</h5>
+        </Link>
+        <button type="button" onClick="">
+          Add to Cart
+        </button>
+        <img height="100px" width="100px" src={props.item.imageUrl} />
+      </div>
+    )
+  }
 }
 
 export default ItemsList
