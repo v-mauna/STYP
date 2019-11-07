c365f73cb989aaf8e259ec599fb39154e9bf896e
Vanessa Mauna <vmauna@aol.com>
2019-11-07 14:26:51 -0500

    fix: corrected some errors in store/index

--- client/components/home.js
+++ client/components/home.js
@@ -1,30 +1,22 @@
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
-import store from '../store/'
 import {fetchAllItems} from '../store/item'
 import ItemsList from './itemsList'
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 
-class Home extends Component {
+class Home extends React.Component {
   componentDidMount() {
-    console.log('Mounted')
-    console.log('Props', this.props)
     this.props.getItems()
   }
   render() {
-    const items = this.props.items
-    console.log('items: ', items)
     return (
       <main>
         <div>
           <h2>shop till you pop</h2>
-          {/* {items.map(item => (
-=======
-          <h2>Shop Till you pop</h2>
-          {/* {this.props.items.map(item => (
-            <ItemsList key={item.id} items={item} />
+          {/* {this.props.items.map((item) => (
+            <ItemsList key={item.id} item={item} />
           ))} */}
         </div>
+        <div />
       </main>
     )
   }
@@ -36,9 +28,7 @@ const mapStateToProps = state => {
   }
 }
 
-const mapDispatchToProps = dispatch => {
-  return {
-    getItems: () => dispatch(fetchAllItems())
-  }
-}
+const mapDispatchToProps = dispatch => ({
+  getItems: () => dispatch(fetchAllItems())
+})
 export default connect(mapStateToProps, mapDispatchToProps)(Home)
