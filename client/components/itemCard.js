f8c91920d072ae649dd2a553f82d87624c4ac610
Vanessa Mauna <53280929+v-mauna@users.noreply.github.com>
2019-11-11 14:27:24 -0500

    Update itemCard.js

--- client/components/itemCard.js
+++ client/components/itemCard.js
@@ -2,23 +2,29 @@ import React from 'react'
 import {Link} from 'react-router-dom'
 
 const ItemCard = props => {
-  const {item: cereal, showDescription} = props
+  const cereal = props.item
   return (
-    <div id="item-card" key={cereal.id}>
+    <div key={cereal.id}>
       <div>
-        <Link to={'/items/' + cereal.id}>
-          <img className="item-image" src={cereal.imageUrl} />
-          <h3 className="card-title">{cereal.name}</h3>
-          {showDescription ? (
-            <p className="card-description">
-              {cereal.description.length < 30
-                ? cereal.description
-                : cereal.description.slice(0, 30) + '...'}
-            </p>
-          ) : (
-            <div />
-          )}
-        </Link>
+        <div className="itemsCard">
+          <Link to={'/cereals/' + cereal.id}>
+            <img src={cereal.imageUrl} />
+            <h3 className="card-title">{cereal.name}</h3>
+          </Link>
+          <p>
+            Price: ${cereal.price}
+            <button
+              id="addCart"
+              type="submit"
+              onClick={() => props.addItem(cereal)}
+            >
+              add to cart
+            </button>
+          </p>
+          <div className="card-content">
+            <p className="card-description" />
+          </div>
+        </div>
       </div>
     </div>
   )
