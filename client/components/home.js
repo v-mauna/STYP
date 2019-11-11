ec6b913a742d96ad116a27c098e289df617989f8
linadinh2602 <51349566+linadinh2602@users.noreply.github.com>
2019-11-11 12:22:21 -0500

    Update home.js

--- client/components/home.js
+++ client/components/home.js
@@ -1,35 +1,61 @@
 import React from 'react'
 import {connect} from 'react-redux'
+import {Link} from 'react-router-dom'
+import ItemCard from './itemCard'
 import {fetchAllItems} from '../store/item'
 
 class Home extends React.Component {
-  componentDidMount() {
-    this.props.getItems()
+    async componentDidMount() {
+    await this.props.fetchItems(this.props.categoryName)
   }
+  
   render() {
-    if (this.props.items.length) {
-      return (
-        <main>
-          <h2>shop till you pop</h2>
-          <div>
-            {this.props.items.map(item => <p key={item.id}>{item.name}</p>)}
+    const {items} = this.props
+    const truncatedItems = items.slice(0, 4)
+    const imgBackground =
+      'https://purewows3.imgix.net/images/articles/2019_07/magic-spoon-fruity-cereal.jpg?auto=format,compress&cs=strip&fit=min&w=728&h=404'
+
+    return (
+      <div>
+        <div className="container">
+          <div
+            id="background-image"
+            style={{backgroundImage: `url(${imgBackground})`, height: '400px'}}
+          />
+          <br />
+
+          <div className="row">
+            <div id="userhome-allProducts-container">
+              <Link id="items-link" to="/items">
+                <h2> See All Products</h2>
+              </Link>
+              <div id="userhome-allProducts">
+                {truncatedItems.map(item => {
+                  return <ItemCard key={item.id} item={item} />
+                })}
+              </div>
+            </div>
+            <br />
+
+            <div id="userhome-bestseller-container">
+              <Link id="bestseller-link" to="/bestsellers">
+                <h2 className="title">Bestsellers</h2>
+              </Link>
+            </div>
           </div>
-          <div />
-        </main>
-      )
-    } else {
-      return <div />
-    }
+        </div>
+      </div>
+    )
   }
 }
 
-const mapStateToProps = state => {
-  return {
-    items: state.itemsReducer
-  }
-}
+const mapStateToProps = state => ({
+  items: state.itemsReducer,
+  categoryName: 'all'
+})
 
 const mapDispatchToProps = dispatch => ({
-  getItems: () => dispatch(fetchAllItems())
+  fetchItems: (categoryName) => dispatch(fetchAllItems(categoryName))
 })
+
 export default connect(mapStateToProps, mapDispatchToProps)(Home)
