8631ec65afc274291ffd9805d0aa8823e0b5869a
Vanessa Mauna <vmauna@aol.com>
2019-11-11 16:49:44 -0500

    Feature: Added checkout-form. Fix: Updates components/index with updated exports

--- client/app.js
+++ client/app.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import {Navbar, Footer} from './components'
+import {Navbar} from './components'
 import Routes from './routes'
 
 const App = () => {
@@ -8,7 +8,7 @@ const App = () => {
     <div>
       <Navbar />
       <Routes />
-      <Footer />
+      {/* <Footer /> */}
     </div>
   )
 }
