b528a933ccb51f9b47c7c6de3103b84c3572f4bb
Vanessa Mauna <53280929+v-mauna@users.noreply.github.com>
2019-11-11 18:35:02 -0500

    Update navbar.js

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -4,87 +4,82 @@ import {Link} from 'react-router-dom'
 import {logout} from '../store'
 
 const Navbar = ({handleClick, isLoggedIn}) => (
-  <div>
-    <div className="navbar-collapse">
-      <p id="navWelcome">
-        <Link to="/home">
-          SHOP TILL YOU POP: Cereal for the <span>not so serious</span>
-        </Link>
-        </h1>
-  
-      <div id="nav-Log-Cart">
-        {isLoggedIn ? (
-          <div className="navbar-items">
-            <Link to="/" className="navbar-items">
-              <h4>{`Welcome!!!!, ${firstName}`}</h4>
-            </Link>
-            <a className="navbar-items" onClick={handleClick}>
-              Logout
-            </a>
-          </div>
-        ) : (
-          <div className="collapse-navbar-collapse">
-            <Link to="/login" className="navbar-items">
-              Login/Register
-            </Link>
-          </div>
-
-        <Link to="/cart" className="navbar-items">
-          Your Cart
-          <img src="https://img.icons8.com/ios-filled/16/000000/shopping-cart.png" />
-          </Link>
-        </div>
-    </div>
-
-    <nav id="sectionsNav">
-      <div className="container">
-        <div className="navbar-header">
-          <Link to="/cereals" className="icon-bar">
-            ALL ITEMS
-          </Link>
-          <Link to="/bestsellers" className="icon-bar">
-            BESTSELLERS
-          </Link>
-          <Link to="/classics" className="icon-bar">
-            CLASSICS
-          </Link>
-          <Link to="/organics" className="icon-bar">
-            ORGANICS
-          </Link>
-          <Link to="the-unknowns" className="icon-bar">
-            THE UNKNOWNS
-          </Link>
-        </div>
-      </div>
-    </nav>
-     <hr />
-  </div>
+ <div>
+   <div className="navbar-collapse">
+     <p id="navWelcome">
+       <Link to="/home">
+         Cereal for the <span>not so serious</span>
+       </Link>
+     </p>
+     {isLoggedIn ? (
+       <div>
+         <Link to="/home">welcome</Link>
+         <a href="#" onClick={handleClick}>
+           {' '}
+           logout
+         </a>
+         <Link to="/cart" className="material-icons">
+           your cart
+           <img src="https://img.icons8.com/ios-filled/16/000000/shopping-cart.png(298 B)
+https://img.icons8.com/ios-filled/16/000000/shopping-cart.png
+" />
+         </Link>
+       </div>
+     ) : (
+       <div className="collapse-navbar-collapse">
+         <Link to="/login" className="material-icons">
+           login/register
+         </Link>
+         <Link to="/cart" className="material-icons">
+           your cart
+           <img src="https://img.icons8.com/ios-filled/16/000000/shopping-cart.png(298 B)
+https://img.icons8.com/ios-filled/16/000000/shopping-cart.png
+" />
+         </Link>
+       </div>
+     )}
+   </div>
+   <nav
+     className="navbar navbar-default navbar-transparent navbar-fixed-top navbar-color-on-scroll"
+     id="sectionsNav"
+   >
+     <div className="container">
+       <div className="navbar-header">
+         <Link to="/cereals" className="icon-bar">
+           ALL ITEMS
+         </Link>
+         <Link to="/bestsellers" className="icon-bar">
+           BESTSELLERS
+         </Link>
+         <Link to="/classics" className="icon-bar">
+           CLASSICS
+         </Link>
+         <Link to="/organics" className="icon-bar">
+           ORGANICS
+         </Link>
+         <Link to="the-unknowns" className="icon-bar">
+           THE UNKNOWNS
+         </Link>
+       </div>
+     </div>
+   </nav>
+ </div>
 )
-
 /* CONTAINER */
 const mapStateToProps = state => {
-  return {
-    isLoggedIn: !!state.user.id,
-    cart: state.cart,
-    user: state.user,
-    items: state.items
-    firstName: state.user.firstName
-  }
+ return {
+   isLoggedIn: !!state.user.id,
+   cart: state.cart,
+   user: state.user,
+   items: state.items
+ }
 }
-
 const mapDispatchToProps = dispatch => {
-  return {
-    handleClick() {
-      dispatch(logout())
-    }
-  }
+ return {
+   handleClick() {
+     dispatch(logout())
+   }
+ }
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(Navbar)
-
-/**
- * PROP TYPES
- */
-Navbar.propTypes = {
-  firstName: PropTypes.string
-}
+export default connect(mapStateToProps,mapDispatchToProps)(Navbar)
