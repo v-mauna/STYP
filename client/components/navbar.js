25ac976bb2a2ba2a41ba3a7f85142504f66bd5ba
linadinh2602 <51349566+linadinh2602@users.noreply.github.com>
2019-11-12 16:08:27 -0500

    Update navbar.js

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -2,49 +2,45 @@ import React from 'react'
 import {connect} from 'react-redux'
 import {Link} from 'react-router-dom'
 import {logout} from '../store'
+import PropTypes from 'prop-types'
 
-const Navbar = ({handleClick, isLoggedIn}) => (
+const Navbar = ({handleClick, isLoggedIn, firstName}) => (
   <div>
     <div className="navbar-collapse">
-      <p id="navWelcome">
-        <Link to="/home">
-          SHOP TILL YOU POP - Cereal for the <span>not so serious</span>
+      <h1 id="navWelcome">
+        <Link to="/">
+          Cereal for the <span>not so serious</span>
         </Link>
-      </p>
-      {isLoggedIn ? (
-        <div>
-          <Link to="/home">welcome</Link>
-          <a href="#" onClick={handleClick}>
-            {' '}
-            logout
-          </a>
-          <Link to="/cart" className="material-icons">
-            your cart
-            <img src="https://img.icons8.com/ios-filled/16/000000/shopping-cart.png" />
-          </Link>
-        </div>
-      ) : (
-        <div className="collapse-navbar-collapse">
-          <Link to="/login" className="material-icons">
-            login/register
-          </Link>
-          <Link to="/cart" className="material-icons">
-            your cart
-            <img src="https://img.icons8.com/ios-filled/16/000000/shopping-cart.png" />
-          </Link>
-        </div>
-      )}
+      </h1>
+
+      <div id="nav-Log-Cart">
+        {isLoggedIn ? (
+          <div className="navbar-items">
+            <Link to="/" className="navbar-items">
+              <h4>{`Welcome!!!!, ${firstName}`}</h4>
+            </Link>
+            <a className="navbar-items" onClick={handleClick}>
+              Logout
+            </a>
+          </div>
+        ) : (
+          <div className="collapse-navbar-collapse">
+            <Link to="/login" className="navbar-items">
+              Login/Register
+            </Link>
+          </div>
+        )}
+
+        <Link to="/cart" className="navbar-items">
+          Your Cart
+          <img src="https://img.icons8.com/ios-filled/16/000000/shopping-cart.png" />
+        </Link>
+      </div>
     </div>
 
-    <nav
-      className="navbar navbar-default navbar-transparent navbar-fixed-top navbar-color-on-scroll"
-      id="sectionsNav"
-    >
+    <nav id="sectionsNav">
       <div className="container">
         <div className="navbar-header">
-          <Link to="/" className="icon-bar">
-            HOME
-          </Link>
           <Link to="/cereals" className="icon-bar">
             ALL ITEMS
           </Link>
@@ -63,6 +59,7 @@ const Navbar = ({handleClick, isLoggedIn}) => (
         </div>
       </div>
     </nav>
+    <hr />
   </div>
 )
 
@@ -72,16 +69,21 @@ const mapStateToProps = state => {
     isLoggedIn: !!state.user.id,
     cart: state.cart,
     user: state.user,
-    items: state.items
+    items: state.items,
+    firstName: state.user.firstName
   }
 }
-
 const mapDispatchToProps = dispatch => {
-  return {
-    handleClick() {
-      dispatch(logout())
-    }
-  }
+ return {
+   handleClick() {
+     dispatch(logout())
+   }
+ }
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(Navbar)
+export default connect(mapStateToProps,mapDispatchToProps)(Navbar)
+
+/** PROP TYPES **/
+Navbar.propTypes = {
+  firstName: PropTypes.string
+}
